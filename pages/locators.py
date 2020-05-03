c7811dc509d03e9c099fe106df3ca23b80c9d6eb
Mikhail <mbalashov84@gmail.com>
2020-05-03 14:38:13 +0300

    refactor all

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,15 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group")
     
 class LoginPageLocators():    
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    
+class ProductPageLocators():
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-safe:first-child")
     
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
+from .basket_page import BasketPage
 from selenium.webdriver.common.by import By
-from .locators import LoginPageLocators
+from .locators import ProductPageLocators
 from selenium.common.exceptions import NoAlertPresentException
 import math
 
@@ -29,9 +30,14 @@ class ProductPage(BasePage):
         try:
             alert = self.browser.switch_to.alert            
             alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            #time.sleep(20)
+            print(f"Your code: {alert_text}")            
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
             
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+       
+    def should_be_is_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "The element is presented, but should not be"
+        
